93dfb57ab2bcc796b0911914f3431797edd738e2
Drew Gates <47280815+drewaggregates@users.noreply.github.com>
2025-02-08 23:59:44 -0500

    finished first challenge

--- Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
+++ Module 1 - Introduction to Java/1. Getting Started/Challenge 1/YourInitials.java
@@ -1,5 +1,11 @@
 public class YourInitials {
     public static void main(String[] args) {
          // Instructions for this workbook are on Learn the Part (See the Udemy Video: Your Initials to access the link).
+         System.out.println("  A      GGG");
+         System.out.println(" A A    G   ");
+         System.out.println("A   A  G   GGG");
+         System.out.println("AAAAA  G     G");
+         System.out.println("A   A   G   G");
+         System.out.println("A   A    GGG");
     }
 }
